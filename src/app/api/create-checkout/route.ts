c496db8f31834631db4d0a0921a4cea674909717
Givanne Nelson <givanne.eth@MacBookPro.lan>
2025-12-10 17:08:32 -0500

    Fix stripe api version mismatch

--- src/app/api/create-checkout/route.ts
+++ src/app/api/create-checkout/route.ts
@@ -18,7 +18,7 @@ if (!getApps().length) {
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-10-29.clover' as any, 
 });
 
 export async function POST(request: NextRequest) {
